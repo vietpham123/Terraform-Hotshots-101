0cccdeecf29e4ac66fa95eb8d7e99ec855485d3a
vietpham123 <calijp123@gmail.com>
2022-02-16 12:10:50 -0600

    added compute manifests

---
+++ Terraform-AWS/Compute/variables.tf
@@ -0,0 +1,16 @@
+################################################
+# Terraform variables                          #
+################################################
+
+variable "access_key" {
+    type = string
+}
+
+variable "secret_key" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
+
