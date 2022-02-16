640a625d931399c5f45360a454ce0f00d2a8c79e
vietpham123 <calijp123@gmail.com>
2022-02-16 12:34:55 -0600

    updated syntax error missing closing " on ingress for port 22

--- Terraform-AWS/Network/main.tf
+++ Terraform-AWS/Network/main.tf
@@ -141,7 +141,7 @@ resource "aws_security_group" "Permit_HTTPS_Any" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0]
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   egress {
