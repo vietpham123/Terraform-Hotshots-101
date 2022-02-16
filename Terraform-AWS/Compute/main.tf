1bf847d1bd7859c12ed2694ce7ac8caab49755a8
vietpham123 <calijp123@gmail.com>
2022-02-16 14:22:46 -0600

    revert

--- Terraform-AWS/Compute/main.tf
+++ Terraform-AWS/Compute/main.tf
@@ -47,8 +47,7 @@ resource "aws_instance" "raddit" {
     sudo apt-get update
     sudo apt-get install -y ruby-full build-essential
     sudo gem install bundler -v "$(grep -A 1 "BUNDLED WITH" ~/var/lib/raddit/Gemfile.lock | tail -n 1)"
-    sudo cd /var/lib/raddit
-    sudo bundle install
+    sudo cd /var/lib/raddit && sudo bundle install && cd
     sudo wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
     sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
     sudo apt-get update
