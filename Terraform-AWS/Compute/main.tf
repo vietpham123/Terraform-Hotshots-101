ea69c9910ff1421cdb5b3165b4c22a4ab1b50538
vietpham123 <calijp123@gmail.com>
2022-02-16 14:10:04 -0600

    updated to elevated user for scripts

--- Terraform-AWS/Compute/main.tf
+++ Terraform-AWS/Compute/main.tf
@@ -44,19 +44,21 @@ resource "aws_instance" "raddit" {
     #!/bin/bash -xe
 
     git clone https://github.com/Artemmkin/raddit.git /var/lib/raddit
-    apt-get update && sudo apt-get install -y ruby-full build-essential
-    gem install bundler -v "$(grep -A 1 "BUNDLED WITH" ~/var/lib/raddit/Gemfile.lock | tail -n 1)"
-    cd /var/lib/raddit && sudo bundle install && cd
-    wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
-    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-    sudo apt-get update && sudo apt-get install -y mongodb-org
+    sudo apt-get update
+    sudo apt-get install -y ruby-full build-essential
+    sudo gem install bundler -v "$(grep -A 1 "BUNDLED WITH" ~/var/lib/raddit/Gemfile.lock | tail -n 1)"
+    sudo cd /var/lib/raddit && sudo bundle install && cd
+    sudo wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+    sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
     sudo systemctl start mongod
     sudo systemctl enable mongod
     sudo wget https://raw.githubusercontent.com/vietpham123/Automation-HotShots/main/raddit.service -P /etc/systemd/system/
     sudo systemctl start raddit
     sudo systemctl enable raddit
     EOL
-    
+
     lifecycle {
         ignore_changes = [ami]
     }
