98f67abc7faf705a21d2d08ba3f206ad97db7b25
vietpham123 <calijp123@gmail.com>
2022-02-16 15:15:13 -0600

    updated user-data

--- Terraform-AWS/Compute/main.tf
+++ Terraform-AWS/Compute/main.tf
@@ -40,24 +40,7 @@ resource "aws_instance" "raddit" {
         Name = var.compute_name
     }
 
-    user_data = << EOF
-#!/bin/bash
-sudo git clone https://github.com/Artemmkin/raddit.git /var/lib/raddit
-sudo apt-get update
-sudo apt-get install -y ruby-full build-essential
-sudo gem install bundler -v "$(grep -A 1 "BUNDLED WITH" ~/var/lib/raddit/Gemfile.lock | tail -n 1)"
-cd /var/lib/raddit
-sudo bundle install && cd
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
-sudo wget https://raw.githubusercontent.com/vietpham123/Automation-HotShots/main/raddit.service -P /etc/systemd/system/
-sudo systemctl start raddit
-sudo systemctl enable raddit
-EOF
+    user_data = "${file("~/scripts/configuration.sh")}"
 
     lifecycle {
         ignore_changes = [ami]
