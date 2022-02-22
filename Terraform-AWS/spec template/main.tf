532fc82429a16feb6a217c11efed14850c90c874
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2022-02-22 15:50:39 -0600

    Update main.tf

--- Terraform-AWS/spec template/main.tf
+++ Terraform-AWS/spec template/main.tf
@@ -14,17 +14,6 @@ data "aws_availability_zones" "AZs" {
 # subnets, security groups.                               #
 ###########################################################
 
-
-###########################################################
-# AWS provider configuration                              #
-###########################################################
-
-provider "aws" {
-    region = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
-}
-
 ###########################################################
 # Create a new VPC, and associate the 10.0.0.0/16 space   #
 ###########################################################
